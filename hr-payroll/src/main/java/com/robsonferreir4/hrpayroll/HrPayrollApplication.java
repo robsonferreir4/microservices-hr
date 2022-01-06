b4eabbdab0b570aa3916fef52baa836c59f23555
Paulo Robson da Silva Ferreira <robson.ferreira@sap.ce.gov.br>
2022-01-05 23:15:38 -0300

    servidor eureka descobrindo projetos worker e payroll

--- hr-payroll/src/main/java/com/robsonferreir4/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/robsonferreir4/hrpayroll/HrPayrollApplication.java
@@ -2,12 +2,12 @@ package com.robsonferreir4.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@EnableCircuitBreaker
-//@EnableEurekaClient
-@RibbonClient(name = "hr-worker")
+@EnableEurekaClient
+//@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
--- hr-worker/src/main/java/com/robsonferreir4/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/robsonferreir4/hrworker/HrWorkerApplication.java
@@ -2,8 +2,9 @@ package com.robsonferreir4.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
