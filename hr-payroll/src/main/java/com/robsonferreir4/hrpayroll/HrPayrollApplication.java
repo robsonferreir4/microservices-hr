a94c19d6697723c25cf4bb238a900ae3c974be97
Paulo Robson da Silva Ferreira <robson.ferreira@sap.ce.gov.br>
2022-01-05 23:34:27 -0300

    hystrix configurações, tolerancia falhas

--- hr-payroll/src/main/java/com/robsonferreir4/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/robsonferreir4/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,11 @@ package com.robsonferreir4.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableCircuitBreaker
+@EnableCircuitBreaker
 @EnableEurekaClient
 //@RibbonClient(name = "hr-worker")
 @EnableFeignClients
