28f7503833a0b09433c0a5ca4dc777898dded3d2
Paulo Robson da Silva Ferreira <robson.ferreira@sap.ce.gov.br>
2022-01-05 22:56:42 -0300

    hrpayment chamando hrworker com duas instancias

--- hr-worker/src/main/java/com/robsonferreir4/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/robsonferreir4/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +25,8 @@ public class WorkerResource {
 //	@Value("${test.config}")
 //	private String testConfig;
 //	
-//	@Autowired
-//	private Environment env;
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private WorkerRepository repository;
@@ -40,6 +39,7 @@ public class WorkerResource {
 //	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
+		logger.info("PORT = " + env.getProperty("local.server.port"));
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
 	}	
@@ -47,13 +47,13 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-//		logger.info("PORT = " + env.getProperty("local.server.port"));
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		
+		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
